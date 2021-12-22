b3155eee1643b860e953000aa9ba49fc48bdf6ea
Haï~ <hai-vr@users.noreply.github.com>
2021-12-22 07:46:40 +0100

    Add Constraint Track Animation components

---
+++ Assets/Hai/ConstraintTrackAnimationCreator/Scripts/Components/ConstraintTrackAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Hai.ConstraintTrackAnimationCreator.Scripts.Components
+{
+    public class ConstraintTrackAnimation : MonoBehaviour
+    {
+        public SingleConstraintTrack[] tracks;
+        public TrackTiming[] timings;
+        public AnimationClip optionalAnimationActive;
+        public AnimationClip optionalAnimationInactive;
+
+        [Serializable]
+        public struct TrackTiming
+        {
+            public float delayStartSeconds;
+            public float scale;
+        }
+    }
+}
---
+++ Assets/Hai/ConstraintTrackAnimationCreator/VRChatSpecific/Scripts/Components/ConstraintTrackAnimationForVRC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Hai.ConstraintTrackAnimationCreator.Scripts.Components;
+using UnityEngine;
+using VRC.SDK3.Avatars.Components;
+
+namespace Hai.ConstraintTrackAnimationCreator.VRChatSpecific.Scripts.Components
+{
+    public class ConstraintTrackAnimationForVRC : MonoBehaviour
+    {
+        public ConstraintTrackAnimation constraintTrackAnimation;
+        public VRCAvatarDescriptor avatar;
+        public string layerName;
+        public CtacVRCFloatType floatType = CtacVRCFloatType.Simple;
+        public bool includeSmoothing;
+
+        [Serializable]
+        public enum CtacVRCFloatType
+        {
+            Simple,
+            Complex,
+            Manual
+        }
+    }
+}
