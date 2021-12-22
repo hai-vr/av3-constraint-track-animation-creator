cafc4e3dfe2bfcbc5c552f41bc43335d707cd3a2
Haï~ <hai-vr@users.noreply.github.com>
2021-12-22 16:25:08 +0100

    By default the animation length will be 10 seconds, to overcome precision issues in the keyframe timing of the animation file (60 frames).

--- Assets/Hai/ConstraintTrackAnimationCreator/Scripts/Components/ConstraintTrackAnimation.cs
+++ Assets/Hai/ConstraintTrackAnimationCreator/Scripts/Components/ConstraintTrackAnimation.cs
@@ -8,7 +8,7 @@ namespace Hai.ConstraintTrackAnimationCreator.Scripts.Components
         public SingleConstraintTrack[] tracks;
         public GameObject parentOfAllTracks;
         public TrackTiming[] optionalTimings;
-        public float globalTimingScale = 1f;
+        public float globalTimingScale = 10f;
         public AnimationClip optionalAnimationActive;
         public AnimationClip optionalAnimationInactive;
 
